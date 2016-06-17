0a365447934be1ecfc4bb17c39d13fc84ea20986
raihan-mpower <raihan@mpower-social.com>
2016-06-17 16:01:21 +0600

    RA|bug number 3 done

--- app/src/main/java/com/iappdragon/recruitmenttest/ThirdBugActivity.java
+++ app/src/main/java/com/iappdragon/recruitmenttest/ThirdBugActivity.java
@@ -2,6 +2,7 @@ package com.iappdragon.recruitmenttest;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.content.res.Configuration;
 import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
@@ -12,11 +13,14 @@ import android.widget.ImageView;
 public class ThirdBugActivity extends Activity {
     Button buttonCapture;
     ImageView imageViewProfile;
+    Bitmap bp;
+    Bundle newBundy;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_thirdbug);
+        newBundy = new Bundle();
         buttonCapture = (Button) findViewById(R.id.buttonCapture);
         imageViewProfile = (ImageView) findViewById(R.id.imageViewProfile);
 
@@ -28,13 +32,37 @@ public class ThirdBugActivity extends Activity {
             }
         });
     }
-
+    Intent databundle;
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         // TODO Auto-generated method stub
         super.onActivityResult(requestCode, resultCode, data);
 
-        Bitmap bp = (Bitmap) data.getExtras().get("data");
+        bp = (Bitmap) data.getExtras().get("data");
+        newBundy.putAll(data.getExtras());
         imageViewProfile.setImageBitmap(bp);
     }
 
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        if (newConfig.orientation == Configuration.ORIENTATION_LANDSCAPE) {
+            onSaveInstanceState(newBundy);
+        } else if (newConfig.orientation == Configuration.ORIENTATION_PORTRAIT) {
+            onSaveInstanceState(newBundy);
+        }
+    }
+    public void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putBundle("newBundy", newBundy);
+    }
+
+    @Override
+    protected void onRestoreInstanceState(Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);        ;
+        bp = (Bitmap) savedInstanceState.getBundle("newBundy").get("data");
+        if(bp!=null) {
+            imageViewProfile.setImageBitmap(bp);
+        }
+
+
+    }
 }
